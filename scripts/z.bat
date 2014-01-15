87d368abb73ecd8cd2050e8edcb6247892b96205
Stefan Scherer <scherer_stefan@icloud.com>
2014-01-15 09:52:49 +0100

    add scripts to user path, added z.bat

---
+++ scripts/z.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if exist %TEMP%\zbat.txt del /F /Q %TEMP%\zbat.txt
+
+call :zdir b:\github %1
+if exist %TEMP%\zbat.txt goto found
+
+
+call :zdir b:\github\vagrant-sandbox %1
+if exist %TEMP%\zbat.txt goto found
+
+call :zdir g:\github %1
+if exist %TEMP%\zbat.txt goto found
+
+goto :EOF
+
+
+:zdir
+set dir=%1
+set substr=%2
+for /F " usebackq delims==" %%i in (`dir /b %dir%`) do (
+  set dn=%%i
+  set tst=%%i
+  set tst=!tst:%substr%=!
+  if not "x!dn!" == "x!tst!" (echo %dir%\!dn! >%TEMP%\zbat.txt & exit /b 42)
+)
+exit /b
+
+:found
+endlocal
+for /f "delims=" %%i in (%TEMP%\zbat.txt) do (set zcd=%%i)
+if exist %TEMP%\zbat.txt del /F /Q %TEMP%\zbat.txt
+cd /D %zcd% 
+goto :EOF
+
--- sync.bat
+++ sync.bat
@@ -1,8 +1,10 @@
 @echo off
-setlocal
 cd /D %~dp0
 git pull
 
+where /q z.bat
+if ERRORLEVEL 1 call :addScriptsToUserPath
+ 
 if "%1x"=="--forcex" goto doIt
 if "%1x"=="-fx" goto doIt
 set /p answer=This may overwrite existing files in your home directory. Are you sure? (y/n)
@@ -12,3 +14,10 @@ goto :EOF
 
 :doIt
 xcopy . "%USERPROFILE%" /EXCLUDE:excludes.txt /s /i /y
+goto :EOF
+
+:addScriptsToUserPath
+for /F "tokens=2* delims= " %%f IN ('reg query "HKCU\Environment" /v Path ^| findstr /i path') do set OLD_USER_PATH=%%g
+setx.exe PATH "%OLD_USER_PATH%;%UserProfile%\scripts" 
+set PATH=%PATH%;%UserProfile%\scripts
+exit /b
