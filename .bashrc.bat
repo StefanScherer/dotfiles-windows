8771c1bfe0d5b21ad41a2cd480d4b0b6abd9c045
Stefan Scherer <scherer_stefan@icloud.com>
2014-01-19 12:31:20 +0100

    .bashrc.bat only runs once

--- .bashrc.bat
+++ .bashrc.bat
@@ -1,10 +1,12 @@
 @echo off
+if "%SKIP_BASHRC_RUN%x"=="1x" goto :EOF
 :: Command shell auto-run
 ::
 :: install this in your %USERPROFILE% and make this an autorun command
 :: with the following registry key:
 ::   reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /d """%UserProfile%\.bashrc.bat""" /f
 ::
+set SKIP_BASHRC_RUN=1
 if "%PROCESSOR_ARCHITECTURE%"=="x86" (
 call "%ProgramFiles%\clink\0.3.1\clink.bat" inject --profile "%LOCALAPPDATA%\clink"
 ) else (
