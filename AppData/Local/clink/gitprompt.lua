d4b4b9d299cae5a2dbf8a20cdaed27383e399074
Stefan Scherer <scherer_stefan@icloud.com>
2014-01-19 17:47:56 +0100

    call git only once in gitprompt
    reduce number of exe files called, checked with procmon.exe

--- AppData/Local/clink/gitprompt.lua
+++ AppData/Local/clink/gitprompt.lua
@@ -1,28 +1,36 @@
 
-function git_prompt_filter()
-    for line in io.popen("git rev-parse --is-inside-work-tree 2>nul"):lines() do
-        local m = line:match("true")
-        if m then
-            for line in io.popen("git branch 2>nul"):lines() do
-                local branch = line:match("%* (.+)$")
-                if branch then
-                    local stat = os.execute("git diff --quiet --ignore-submodules HEAD >nul")
-                    if stat then
-                        dirty = ""
-                    else
-                        dirty = "*"
-                    end
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
 
-                    local prompt = clink.prompt.value:match("(.*)>$")
-                    if prompt then
-                        clink.prompt.value = prompt.." ["..branch..dirty.."]>"
-                    end
-                    break
-                end
-            end
+function git_prompt_filter()
+    local branch
+    local dirty = ""
+
+    local git = "c:/git/bin/git.exe" -- this will reduce the number of exe files called, checked with procmon.exe
+    if not file_exists(git)
+    then
+        git = "git"
+    end
+       
+    for line in io.popen(git.." status 2>nul"):lines() do
+        m = line:match("^# On branch (.+)$")
+        if m then
+            branch = m
+        end
+        m = line:match("modified:")
+        if m then
+            dirty = "*"
+            break
         end
     end
-
+    if branch then
+        local prompt = clink.prompt.value:match("(.*)>$")
+        if prompt then
+            clink.prompt.value = prompt.." ["..branch..dirty.."]>"
+        end
+    end
 
     return false
 end
