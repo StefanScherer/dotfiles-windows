e794c4da337d398344307b50d2837b6307da801c
Stefan Scherer <scherer_stefan@icloud.com>
2014-01-23 20:48:31 +0100

    check if clink dir exists

--- .bashrc.bat
+++ .bashrc.bat
@@ -7,10 +7,12 @@ if "%SKIP_BASHRC_RUN%x"=="1x" goto :EOF
 ::   reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /d """%UserProfile%\.bashrc.bat""" /f
 ::
 set SKIP_BASHRC_RUN=1
-if "%PROCESSOR_ARCHITECTURE%"=="x86" (
-call "%ProgramFiles%\clink\0.3.1\clink.bat" inject --profile "%LOCALAPPDATA%\clink"
-) else (
-call "%ProgramFiles(x86)%\clink\0.3.1\clink.bat" inject --profile "%LOCALAPPDATA%\clink"
+if exist "%ProgramFiles%\clink\0.3.1\" (
+  if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+  call "%ProgramFiles%\clink\0.3.1\clink.bat" inject --profile "%LOCALAPPDATA%\clink"
+  ) else (
+  call "%ProgramFiles(x86)%\clink\0.3.1\clink.bat" inject --profile "%LOCALAPPDATA%\clink"
+  )
 )
 doskey /MACROFILE=%~dp0\.aliases
 
