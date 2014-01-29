f93b79a98146b39056493b411ca07fc831ee2822
unknown <Administrator@dc.sealsystems.local>
2014-01-29 10:28:27 +0100

    make it compatible with cinst vim

--- scripts/viewpath.bat
+++ scripts/viewpath.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal
+set vi="C:\Program Files (x86)\vim\vim74\gvim.exe"
+if exist "C:\Program Files (x86)\vim\vim74\gvim.exe" goto :viewpath
 set vi=vi
 where /q vi
 if ERRORLEVEL 1 set vi=gvim
+:viewpath
 path | %vi% - -c :s/PATH=// -c :s/;/\r/g
