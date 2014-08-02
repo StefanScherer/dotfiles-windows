67b164b99c8c3fca493770ce1a5a4290a078aa7d
Stefan Scherer <scherer_stefan@icloud.com>
2014-08-02 17:22:31 +0200

    improved vagrant-vmware-shell, keep current dir

--- bin/vagrant-vmware-shell.bat
+++ bin/vagrant-vmware-shell.bat
@@ -1,7 +1,11 @@
 @echo off
 set VAGRANT_HOME=%~dp0\..\.vagrant.d
 set ChocolateyInstall=C:\ProgramData\Chocolatey
-set PATH=%PATH%;%ChocolateyInstall%\bin;%~dp0\..\bin
-cd /D %~dp0\..
+set PATH=%PATH%;%ChocolateyInstall%\bin;%~dp0\..\bin;C:\git\cmd;C:\git\bin
+if "%1x"=="x" (
+  cd /D %~dp0\..
+) else (
+  cd /D "%1"
+)
 echo.
 echo Now you can use vagrant for VMware Workstation provider.
