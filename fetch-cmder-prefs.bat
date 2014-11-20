c18a98cbb075fd36dee4a30ef68aabffe85969f0
Stefan Scherer <scherer_stefan@icloud.com>
2014-11-20 10:26:22 +0100

    skip .history

--- fetch-cmder-prefs.bat
+++ fetch-cmder-prefs.bat
@@ -2,3 +2,4 @@
 rem This is a helper to fetch my cmder prefs into the repo for commit and push
 rem cmder is previously installed with `cinst cmder`
 copy "C:\ProgramData\Chocolatey\lib\cmder.portable.1.1.1\tools\cmder\config\*" "%~dp0\cmder\config\
+del /F "%~dp0\cmder\config\.history
