5e90d39ead40c1d1f0bc1a69da584f6264641b1f
Stefan Scherer <scherer_stefan@icloud.com>
2018-01-19 13:40:15 +0000

    Install modules z and posh-git

--- sync.ps1
+++ sync.ps1
@@ -2,4 +2,13 @@ if (!(Test-Path $env:USERPROFILE\Documents\WindowsPowerShell)) {
   mkdir $env:USERPROFILE\Documents\WindowsPowerShell
 }
 
+if (!(Test-Path $env:USERPROFILE\Documents\WindowsPowerShell\Modules\z)) {
+  Install-Module z -Scope CurrentUser -AllowClobber
+}
+
+if (!(Test-Path $env:USERPROFILE\Documents\WindowsPowerShell\Modules\posh-git)) {
+  Install-Module posh-git -Scope CurrentUser
+}
+
+
 copy Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
