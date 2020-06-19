a25427b2958a5e9daa32b44559c1bddaa2d622e1
Stefan Scherer <stefan.scherer@docker.com>
2020-06-19 19:21:05 +0200

    Show root, add ctrl+a, ctrl+e

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,5 @@
 ﻿import-module z
 
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-Set-PSReadlineOption -BellStyle None
-
 function .. { cd .. }
 function ... { cd .. ; cd .. }
 function .... { cd .. ; cd .. ; cd .. }
@@ -12,6 +7,20 @@ function ..... { cd .. ; cd .. ; cd .. ; cd .. }
 function home { cd $env:USERPROFILE }
 
 Import-Module posh-git
+
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord Ctrl+a -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Chord Ctrl+e -Function EndOfLine
+Set-PSReadLineKeyHandler -Chord Alt+LeftArrow -Function BackwardWord
+Set-PSReadLineKeyHandler -Chord Alt+RightArrow -Function NextWord
+Set-PSReadlineOption -BellStyle None
+
+function Test-Administrator
+{
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
 
 # Set up a simple prompt, adding the git prompt parts inside git repos
 function global:prompt {
@@ -22,6 +31,9 @@ function global:prompt {
     Write-VcsStatus
     Write-Host
     $global:LASTEXITCODE = $realLASTEXITCODE
+    if (Test-Administrator) {
+        Write-Host -NoNewline -ForegroundColor Red "Administrator $env:USERNAME "
+    }
     return "$ "
 }
 
@@ -29,4 +41,4 @@ function t {
     wsl.exe -d Ubuntu-20.04 ~/bin/t
 }
 
-Start-SshAgent -Quiet
+# Start-SshAgent -Quiet
